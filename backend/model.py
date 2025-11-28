17ab36de1bc2ebcdf64ddf43b3a2363e45c8fbe5
apricott4113 <sean0411sean@gmail.com>
2025-11-28 11:02:07 +0900

    .

---
+++ backend/model.py
@@ -0,0 +1,32 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import pandas as pd
+import torch
+from torchvision import transforms
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    ),
+])
+
+class MPIIGazeDataset(Dataset):
+    def __init__(self, csv_path, transform=None):
+        self.df = pd.read_csv(csv_path)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        img = Image.open(row["image_path"]).convert("RGB")
+        if self.transform:
+            img = self.transform(img)
+        gaze = torch.tensor([row["gx"], row["gy"]], dtype=torch.float32)
+        return img, gaze
+    
+    
